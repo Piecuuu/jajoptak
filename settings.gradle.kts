2f2fe5454d84f5d24ff07d8e243eec180dbbcb3b
Piecuuu <56731916+Piecuuu@users.noreply.github.com>
2026-02-08 19:15:49 +0100

    internal rebrand

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -33,7 +33,7 @@ if (!file(".git").exists()) {
     error(errorText)
 }
 
-rootProject.name = "leaf"
+rootProject.name = "jajoptak"
 
 for (name in listOf("leaf-api", "leaf-server")) {
     val projName = name.lowercase(Locale.ENGLISH)
